23eeaf92fea9ee5073476fc84329ba903caca4e5
JackyLin <Yw020416@gmail.com>
2021-01-19 13:15:17 +0800

    更改了端口号

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://192.168.178.52:5000/api`,
-        target: `http://www.cdb99.com:5008/api/`,
+        target: `http://www.cdb99.com:5010/api/`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
