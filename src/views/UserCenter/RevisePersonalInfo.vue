999608a281b5dbd26d138eb600cea287c76a402b
JackyLin <Yw020416@gmail.com>
2021-01-20 10:19:40 +0800

    对接了个人信息的api接口

--- src/views/UserCenter/RevisePersonalInfo.vue
+++ src/views/UserCenter/RevisePersonalInfo.vue
@@ -2,17 +2,21 @@
   <div style="background-color:#797979">
     <van-nav-bar :title="$route.params.params" left-text="返回" left-arrow @click-left="onClickLeft"/>
     <van-cell-group>
-      <van-cell v-for="(item,index) in title" :key="index" clickable is-link center>
-        <template #title>
-          {{item}}
-        </template>
-      </van-cell>
+      <van-cell title="头像" :value="content.photo" clickable is-link center/>
+      <van-cell title="昵称" :value="content.username" clickable is-link center/>
+      <van-cell title="性别" :value="content.username" clickable is-link center/>
+      <van-cell title="生日" :value="content.birthday" clickable is-link center/>
+      <van-cell title="所在地" :value="content.region" clickable is-link center/>
+      <van-cell title="简介" :value="content.intro" clickable is-link center/>
+      <van-cell title="学校" :value="content.education" clickable is-link center/>
+      <van-cell title="公司" :value="content.company" clickable is-link center/>
     </van-cell-group>
   </div>
 </template>
 
 <script>
-import { Col, Row, Cell, CellGroup, NavBar }from 'vant'
+import { Col, Row, Cell, CellGroup, NavBar } from 'vant'
+import { getInfo } from '@/api/user.js'
 
 export default {
   name:'AdressInConfirmOrder',
@@ -27,14 +31,24 @@ export default {
 
   data(){
     return{
-      title:['头像','昵称','性别','生日','所在地','简介','学校','公司']
+      content:{}
     }
   },
 
   methods:{
-     onClickLeft(){
+    onClickLeft(){
        this.$router.push({name:'Me'})
+    },
+
+    async getInfo(){
+      const response = await getInfo()
+      this.content = response.data
+      console.log(this.content);
     }
+  },
+
+  created(){
+    this.getInfo()
   }
 }
 </script>
